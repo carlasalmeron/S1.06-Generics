e28ccf0eb5e934c7f18748269f9a9c4e883370b2
Carla Salmerón <salmeroncarla98@gmail.com>
2026-02-25 11:18:01 +0100

    feat: apply corrections

--- Level1&2/src/main/java/task1task2/GenericMethods.java
+++ Level1&2/src/main/java/task1task2/GenericMethods.java
@@ -2,22 +2,26 @@ package task1task2;
 
 public class GenericMethods {
 
-    public static <T, U, V> void printElements (T element1, U element2, V element3) {
+    public static <T, U, V> void printElements(T element1, U element2, V element3) {
         System.out.println(element1 + "\n" +
                 element2 + "\n" +
                 element3);
     }
 
-    public static <T, U> void printElements (String message, T element1, U element2) {
-        System.out.println("Message: "+ message + "\n" +
+    public static <T, U> void printElements(String message, T element1, U element2) {
+        System.out.println("Message: " + message + "\n" +
                 element1 + "\n" +
                 element2);
     }
 
     @SafeVarargs
-    public static <T> void printAll (T... elements) {
+    public static <T> void printAll(T... elements) {
         for (T element : elements) {
             System.out.println(element);
         }
+    }
+
+    public static <T extends Number> void printNumber(T number) {
+        System.out.println("The number is: " + number.doubleValue());
     }
 }
--- Level1&2/src/main/java/task1task2/Main.java
+++ Level1&2/src/main/java/task1task2/Main.java
@@ -3,20 +3,29 @@ package task1task2;
 public class Main {
     public static void main(String[] args) {
 
-        Person person = new Person("Carla","Salmerón",27);
-        Person person1 = new Person("Milagros","Bermúdez",39);
+        System.out.println("----- Record Person -----");
+        Person person = new Person("Carla", "Salmerón", 27);
+        Person person1 = new Person("Milagros", "Bermúdez", 39);
+        System.out.println(person);
+        System.out.println("Name: " + person.name());
 
-        System.out.println("-----Generic Method with Multiple Parameters-----");
+        System.out.println("\n----- Generic Method with Multiple Parameters -----");
         GenericMethods.printElements(2026, "First Test", person);
 
-        System.out.println("\n-----Partial Generic Method-----");
+        System.out.println("\n----- Partial Generic Method -----");
         GenericMethods.printElements("Second Test", person1, 100);
         System.out.println();
         GenericMethods.printElements("Third Test", 3.5, "Hello");
 
-        System.out.println("\n-----Generic Varargs Method-----");
-        GenericMethods.printAll(person, person1);
-        System.out.println();
-        GenericMethods.printAll(1, 2, 3, 4, 5);
+        System.out.println("\n----- Generic Varargs Method -----");
+        GenericMethods.printAll(person, person1, "More elements", 42);
+
+        System.out.println("\n----- Bounded Generic Method (Numbers Only) -----");
+        GenericMethods.printNumber(10);
+        GenericMethods.printNumber(15.5);
+
+        System.out.println("\n----- Non-Generic Comparison -----");
+        NoGenericMethods noGen = new NoGenericMethods("E1", "E2", "E3");
+        System.out.println(noGen);
     }
 }
